26cb58822501d35de14de70eda4cf862c4e2d58f
kartiksahu55 <kartiksahu55@gmail.com>
2024-07-10 13:12:56 +0530

    Deployment Testing

--- client/src/apiDetails.js
+++ client/src/apiDetails.js
@@ -1,31 +1,31 @@
 // *****************************************************
-export const SEND_OTP_API = `https://helixcrmserver-ki70cbiz6-kartiksahu55s-projects.vercel.app//api/v1/user/otprequest`;
-export const SIGNUP_API = `https://helixcrmserver-ki70cbiz6-kartiksahu55s-projects.vercel.app//api/v1/user/signup`;
-export const Update_USER_Details_API = `https://helixcrmserver-ki70cbiz6-kartiksahu55s-projects.vercel.app//api/v1/user/update`;
-export const SIGNIN_API = `https://helixcrmserver-ki70cbiz6-kartiksahu55s-projects.vercel.app//api/v1/user/login`;
-export const LOGOUT_API = `https://helixcrmserver-ki70cbiz6-kartiksahu55s-projects.vercel.app//api/v1/user/logout`;
-export const FETCH_USER_DETAILS = `https://helixcrmserver-ki70cbiz6-kartiksahu55s-projects.vercel.app//api/v1/user/fetch`;
+export const SEND_OTP_API = `https://helixcrmserver.vercel.app/api/v1/user/otprequest`;
+export const SIGNUP_API = `https://helixcrmserver.vercel.app/api/v1/user/signup`;
+export const Update_USER_Details_API = `https://helixcrmserver.vercel.app/api/v1/user/update`;
+export const SIGNIN_API = `https://helixcrmserver.vercel.app/api/v1/user/login`;
+export const LOGOUT_API = `https://helixcrmserver.vercel.app/api/v1/user/logout`;
+export const FETCH_USER_DETAILS = `https://helixcrmserver.vercel.app/api/v1/user/fetch`;
 
 // Employee API
-export const CREATE_EMPLOYEE_API = `https://helixcrmserver-ki70cbiz6-kartiksahu55s-projects.vercel.app//api/v1/user/createemployee`;
-export const FETCH_EMPLOYEE_DATA_API = `https://helixcrmserver-ki70cbiz6-kartiksahu55s-projects.vercel.app//api/v1/user/fetchemployee`;
-export const UPDATE_EMPLOYEE_DATA_API = `https://helixcrmserver-ki70cbiz6-kartiksahu55s-projects.vercel.app//api/v1/user/updateemployee`;
+export const CREATE_EMPLOYEE_API = `https://helixcrmserver.vercel.app/api/v1/user/createemployee`;
+export const FETCH_EMPLOYEE_DATA_API = `https://helixcrmserver.vercel.app/api/v1/user/fetchemployee`;
+export const UPDATE_EMPLOYEE_DATA_API = `https://helixcrmserver.vercel.app/api/v1/user/updateemployee`;
 
 // User Avatar
-export const UPLOAD_USER_AVATAR_API = `https://helixcrmserver-ki70cbiz6-kartiksahu55s-projects.vercel.app//api/v1/user/uploaduseravatar`;
+export const UPLOAD_USER_AVATAR_API = `https://helixcrmserver.vercel.app/api/v1/user/uploaduseravatar`;
 
 // API to handle Leads
-export const CREATE_QUICK_LEAD_API = `https://helixcrmserver-ki70cbiz6-kartiksahu55s-projects.vercel.app//api/v1/leads/create`;
-export const FETCH_LEADS_DATA_API = `https://helixcrmserver-ki70cbiz6-kartiksahu55s-projects.vercel.app//api/v1/leads/fetch`;
-export const DELETE_LEAD_API = `https://helixcrmserver-ki70cbiz6-kartiksahu55s-projects.vercel.app//api/v1/leads/delete`;
-export const UPDATE_LEAD_API = `https://helixcrmserver-ki70cbiz6-kartiksahu55s-projects.vercel.app//api/v1/leads/update`;
-export const ASSIGN_LEAD_API = `https://helixcrmserver-ki70cbiz6-kartiksahu55s-projects.vercel.app//api/v1/leads/assignleads`;
+export const CREATE_QUICK_LEAD_API = `https://helixcrmserver.vercel.app/api/v1/leads/create`;
+export const FETCH_LEADS_DATA_API = `https://helixcrmserver.vercel.app/api/v1/leads/fetch`;
+export const DELETE_LEAD_API = `https://helixcrmserver.vercel.app/api/v1/leads/delete`;
+export const UPDATE_LEAD_API = `https://helixcrmserver.vercel.app/api/v1/leads/update`;
+export const ASSIGN_LEAD_API = `https://helixcrmserver.vercel.app/api/v1/leads/assignleads`;
 
 // Sales Api
-export const CREATE_SALES_API = `https://helixcrmserver-ki70cbiz6-kartiksahu55s-projects.vercel.app//api/v1/sales/create`;
-export const FETCH_SALES_DATA_API = `https://helixcrmserver-ki70cbiz6-kartiksahu55s-projects.vercel.app//api/v1/sales/fetch`;
-export const UPDATE_SALES_DATA_API = `https://helixcrmserver-ki70cbiz6-kartiksahu55s-projects.vercel.app//api/v1/sales/update`;
-export const DELETE_SALES_DATA_API = `https://helixcrmserver-ki70cbiz6-kartiksahu55s-projects.vercel.app//api/v1/sales/delete`;
+export const CREATE_SALES_API = `https://helixcrmserver.vercel.app/api/v1/sales/create`;
+export const FETCH_SALES_DATA_API = `https://helixcrmserver.vercel.app/api/v1/sales/fetch`;
+export const UPDATE_SALES_DATA_API = `https://helixcrmserver.vercel.app/api/v1/sales/update`;
+export const DELETE_SALES_DATA_API = `https://helixcrmserver.vercel.app/api/v1/sales/delete`;
 
 
 // // *****************************************************
